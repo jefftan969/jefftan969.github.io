3edd9406339faaca3dbf38d539f1d40f1a6c0efa
Jeff Tan <jefftan969@gmail.com>
2024-04-16 19:11:01 -0400

    try alternative alpha blending

--- 3dgs/index.js
+++ 3dgs/index.js
@@ -173,7 +173,7 @@ AFRAME.registerComponent("gaussian_splatting", {
 					float A = -dot(vPosition, vPosition);
 					if (A < -4.0) discard;
 					float B = exp(A) * vColor.a;
-					gl_FragColor = vec4(vColor.rgb, B);
+					gl_FragColor = vec4(B * vColor.rgb, B);
 				}
 			`,
 			blending : THREE.CustomBlending,
