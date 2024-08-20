8dab92069dcf05bf7eeff9ff7678695074a056fa
Jeff Tan <jefftan969@gmail.com>
2024-08-20 10:23:52 -0400

    update dressrecon website

--- dressrecon/js/app.js
+++ dressrecon/js/app.js
@@ -1,93 +1,98 @@
 
 $(document).ready(function() {
-    const sequences = [
-        "0008_01", "0047_01", "0047_12", "0102_02", "0113_06", "0121_02", "0123_02",
-        "0128_04", "0133_07", "0152_01", "0166_04", "0188_02", "0206_04", "0239_01",
+    const results_sequences = [
+        "shape", "tracks", "gaussians", "rgb", "ref_rgb",
     ];
-    window.baseline_comp_seq = "0123_02";
-    window.deform_vis_seq = "0008_01";
+    const baseline_comparison_sequences = [
+        "0008_01", "0102_02", "0113_06", "0121_02", "0123_02",
+        "0128_04", "0152_01", "0166_04", "0188_02", "0239_01",
+    ];
+    const motion_decomposition_sequences = [
+        "0008_01", "0102_02", "0113_06", "0121_02", "0123_02",
+        "0128_04", "0152_01", "0166_04", "0188_02", "0239_01",
+    ];
+    const extreme_view_synthesis_sequences = [
+        "0102_02", "0121_02", "0128_04", "0188_02",
+    ];
+    window.results_seq = "gaussians";
+    window.baseline_comparison_seq = "0123_02";
+    window.motion_decomposition_seq = "0008_01";
+    window.extreme_view_synthesis_seq = "0121_02";
 
-    const views = ["deg0", "deg-90", "deg90"];
-    window.baseline_comp_view = "deg0";
-    window.deform_vis_view = "deg0";
+    // ===== Results: Sequence Selector
+    function get_onclick_results(seq) {
+        return function(e) {
+            window.results_seq = seq;
+            document.getElementById("results").src = "./img/results/" + seq + ".mp4";
+            for (var i = 0; i < results_sequences.length; i++) {
+                var other_seq = results_sequences[i];
+                document.getElementById("results_" + other_seq).className = "";
+            }
+            document.getElementById("results_" + seq).className = "selected-scene";
+        };
+    }
+
+    for (var i = 0; i < results_sequences.length; i++) {
+        var seq = results_sequences[i];
+        document.getElementById("results_" + seq).addEventListener("click", get_onclick_results(seq));
+    }
+    get_onclick_results(window.results_seq)("");
 
     // ===== Baseline Comparison: Sequence Selector
-    function get_onclick_baseline_comp_seq(seq) {
+    function get_onclick_baseline_comparison(seq) {
         return function(e) {
-            window.baseline_comp_seq = seq;
-            var view = window.baseline_comp_view;
-            document.getElementById("baseline_comp_video").src = "./img/baseline_comp/" + view + "/" + seq + ".mp4";
-            for (var i = 0; i < sequences.length; i++) {
-                var other_seq = sequences[i];
-                document.getElementById("baseline_comp_" + other_seq).className = "";
+            window.baseline_comparison_seq = seq;
+            document.getElementById("baseline_comparison").src = "./img/baseline_comparison/" + seq + ".mp4";
+            for (var i = 0; i < baseline_comparison_sequences.length; i++) {
+                var other_seq = baseline_comparison_sequences[i];
+                document.getElementById("baseline_comparison_" + other_seq).className = "";
             }
-            document.getElementById("baseline_comp_" + seq).className = "selected-scene";
+            document.getElementById("baseline_comparison_" + seq).className = "selected-scene";
         };
     }
 
-    for (var i = 0; i < sequences.length; i++) {
-        var seq = sequences[i];
-        document.getElementById("baseline_comp_" + seq).addEventListener("click", get_onclick_baseline_comp_seq(seq));
+    for (var i = 0; i < baseline_comparison_sequences.length; i++) {
+        var seq = baseline_comparison_sequences[i];
+        document.getElementById("baseline_comparison_" + seq).addEventListener("click", get_onclick_baseline_comparison(seq));
     }
-    get_onclick_baseline_comp_seq(window.baseline_comp_seq)("");
+    get_onclick_baseline_comparison(window.baseline_comparison_seq)("");
 
-    // ===== Baseline Comparison: View Selector
-    function get_onclick_baseline_comp_view(view) {
+    // ===== Motion Decomposition: Sequence Selector
+    function get_onclick_motion_decomposition(seq) {
         return function(e) {
-            window.baseline_comp_view = view;
-            var seq = window.baseline_comp_seq;
-            document.getElementById("baseline_comp_video").src = "./img/baseline_comp/" + view + "/" + seq + ".mp4";
-            for (var i = 0; i < views.length; i++) {
-                var other_view = views[i];
-                document.getElementById("baseline_comp_" + other_view).className = "";
+            window.motion_decomposition_seq = seq;
+            document.getElementById("motion_decomposition").src = "./img/motion_decomposition/" + seq + ".mp4";
+            for (var i = 0; i < motion_decomposition_sequences.length; i++) {
+                var other_seq = motion_decomposition_sequences[i];
+                document.getElementById("motion_decomposition_" + other_seq).className = "";
             }
-            document.getElementById("baseline_comp_" + view).className = "selected-scene";
+            document.getElementById("motion_decomposition_" + seq).className = "selected-scene";
         };
     }
 
-    for (var i = 0; i < views.length; i++) {
-        var view = views[i];
-        document.getElementById("baseline_comp_" + view).addEventListener("click", get_onclick_baseline_comp_view(view));
+    for (var i = 0; i < motion_decomposition_sequences.length; i++) {
+        var seq = motion_decomposition_sequences[i];
+        document.getElementById("motion_decomposition_" + seq).addEventListener("click", get_onclick_motion_decomposition(seq));
     }
-    get_onclick_baseline_comp_view(window.baseline_comp_view)("");
+    get_onclick_motion_decomposition(window.motion_decomposition_seq)("");
 
-    // ===== Deformation Visualization: Sequence Selector
-    function get_onclick_deform_vis_seq(seq) {
+    // ===== Extreme View Synthesis: Sequence Selector
+    function get_onclick_extreme_view_synthesis(seq) {
         return function(e) {
-            window.deform_vis_seq = seq;
-            var view = window.deform_vis_view;
-            document.getElementById("deform_vis_video").src = "./img/deform_vis/" + view + "/" + seq + ".mp4";
-            for (var i = 0; i < sequences.length; i++) {
-                var other_seq = sequences[i];
-                document.getElementById("deform_vis_" + other_seq).className = "";
+            window.extreme_view_synthesis_seq = seq;
+            document.getElementById("extreme_view_synthesis").src = "./img/extreme_view_synthesis/" + seq + ".mp4";
+            for (var i = 0; i < extreme_view_synthesis_sequences.length; i++) {
+                var other_seq = extreme_view_synthesis_sequences[i];
+                document.getElementById("extreme_view_synthesis_" + other_seq).className = "";
             }
-            document.getElementById("deform_vis_" + seq).className = "selected-scene";
+            document.getElementById("extreme_view_synthesis_" + seq).className = "selected-scene";
         };
     }
 
-    for (var i = 0; i < sequences.length; i++) {
-        var seq = sequences[i];
-        document.getElementById("deform_vis_" + seq).addEventListener("click", get_onclick_deform_vis_seq(seq));
+    for (var i = 0; i < extreme_view_synthesis_sequences.length; i++) {
+        var seq = extreme_view_synthesis_sequences[i];
+        document.getElementById("extreme_view_synthesis_" + seq).addEventListener("click", get_onclick_extreme_view_synthesis(seq));
     }
-    get_onclick_deform_vis_seq(window.deform_vis_seq)("");
+    get_onclick_extreme_view_synthesis(window.extreme_view_synthesis_seq)("");
 
-    // ===== Gaussian Visualization: View Selector
-    function get_onclick_deform_vis_view(view) {
-        return function(e) {
-            window.deform_vis_view = view;
-            var seq = window.deform_vis_seq;
-            document.getElementById("deform_vis_video").src = "./img/deform_vis/" + view + "/" + seq + ".mp4";
-            for (var i = 0; i < views.length; i++) {
-                var other_view = views[i];
-                document.getElementById("deform_vis_" + other_view).className = "";
-            }
-            document.getElementById("deform_vis_" + view).className = "selected-scene";
-        };
-    }
-
-    for (var i = 0; i < views.length; i++) {
-        var view = views[i];
-        document.getElementById("deform_vis_" + view).addEventListener("click", get_onclick_deform_vis_view(view));
-    }
-    get_onclick_deform_vis_view(window.deform_vis_view)("");
 });
